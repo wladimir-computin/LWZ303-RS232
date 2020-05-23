a26b176013b5ec6bcb2d05e1698e71733959c1e5
David Wischnjak <davidw.software@gmail.com>
2020-05-23 17:07:52 +0200

    updated daemon

--- daemon.py
+++ daemon.py
@@ -8,6 +8,7 @@ import time
 import signal
 import json
 from json import JSONEncoder
+import os
 
 def _default(self, obj):
     return getattr(obj.__class__, "__json__", _default.default)(obj)
@@ -19,32 +20,50 @@ JSONEncoder.default = _default
 
 STUFF = [sGlobalGroup, sControlGroup, sHC1Group, sDhwGroup, sSolarGroup, sFanGroup, sProgramGroup, sHistoryGroup]
 
+class Logger:
+	
+	def start(self):
+		trans = TransportTCP("192.168.178.201", 7777)
+		#self.trans = TransportSerial("/dev/ttyUSB0", 9600)
+		self.comm = Communicator(trans)
+		self.comm.start()
+		self.w = Wrapper(self.comm)
+		
+	def loop(self):
+		filename = time.strftime("%Y.%m.%d_%H:%M:%S.json", time.localtime())
+		with open(f"log/{filename}", "w") as f:
+			groups = self.w.getBulkGroups(STUFF)
+			for k,v in groups.items():
+				print(v)
+			json.dump(groups, f, indent=4)
+		
+	def end(self):
+		self.comm.stop()
+
+logger = None
 
 def main():
 	signal.signal(signal.SIGINT, handler)
-	trans = TransportTCP("192.168.178.201", 7777)
-	#trans = TransportSerial("/dev/ttyUSB0", 9600)
-	comm = Communicator(trans)
-	comm.start()
-	w = Wrapper(comm)
+	global logger
+	logger = Logger()
+	logger.start()
 	while True:
 		try:
-			time.sleep(60)
-			filename = time.strftime("%Y.%m.%d_%H:%M:%S.json", time.localtime())
-			with open(f"log/{filename}", "w") as f:
-				groups = w.getBulkGroups(STUFF)
-				for k,v in groups.items():
-					print(v)
-				json.dump(groups, f, indent=4)
+			logger.loop()
+			time.sleep(60 - time.localtime().tm_sec)
 		except Exception as e:
 			print(e)
+			time.sleep(5)
+			os.system("find ./log/ -type f -empty -delete")
 	
-	comm.stop()
+
 	
 def handler(signum, frame):
 	exit()
 
 def exit():
+	global logger
+	logger.end()
 	quit()
 
 if __name__== "__main__":
